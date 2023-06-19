30d9b11ef13115de7b764acc07f761be37c1b7e6
seregAnt02 <129961993+seregAnt02@users.noreply.github.com>
2023-06-19 17:28:10 +0500

    Update Program.cs

--- Task/Program.cs
+++ Task/Program.cs
@@ -5,36 +5,8 @@
 При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 */
 
-//РЕШЕНИЕ 1 (с использованием двух массивов)
-Console.Write("Сколько элементов Вы хотите ввести? ");
-int size = Convert.ToInt32(Console.ReadLine());
+// РЕШЕНИЕ 
 
-string[] arrayStrings = new string[size];
-for (int i = 0; i < size; i++)
-{
-    Console.WriteLine($"Введите {i+1}-й элемент: ");
-    string element = Convert.ToString(Console.ReadLine());
-    arrayStrings[i] = element;
-}
-
-string[] arrayFinal = new string[size];
-int len = 3;
-int pos = 0;
-
-for (int j = 0; j < size; j++)
-{
-    if (arrayStrings[j].Length <= len)
-    {
-        arrayFinal[pos] = arrayStrings[j];
-        pos++;
-    }
-}
-
-Console.WriteLine();
-PrintArray(arrayFinal);
-
-// РЕШЕНИЕ 2 (с использованием одного массива)
-/* 
 Console.Write("Сколько элементов Вы хотите ввести? ");
 int size = Convert.ToInt32(Console.ReadLine());
 
@@ -56,7 +28,7 @@ for (int i = 0; i < size; i++)
 
 Console.WriteLine();
 PrintArray(arrayStrings);
-*/
+
 
 // _____________Блок вспомогательных методов_____________
 //
